dc159a006ba580004774725d70c53e0da2483721
Michael Dao <michaeltuandao@gmail.com>
2019-07-11 13:50:18 -0500

    setup

---
+++ heroes/superheroes-movies/components/AddMovie.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class AddMovie extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+
+        return(
+            <div>
+                <ul>
+                    <li>Hi</li>
+                </ul>
+            </div>
+        );
+        
+    }
+}
+
+export default AddMovie
